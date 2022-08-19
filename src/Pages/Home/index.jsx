313199f8f60ccce759bfd0642aa717c9bdd28fe1
Charlie Bellow <92690276+CharlieBellow@users.noreply.github.com>
2022-08-19 11:42:44 -0300

    imutabilidade

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,5 @@
 
-import react, { useState } from 'react'
+import React, { useState } from 'react' //hook: funções que permitem conectar o estado com o ciclo de vida dos componentes funcionais
 
 import './style.css'
 
@@ -8,12 +8,33 @@ import {Card} from '../../components/Card'
 export function Home() {
 
   const [studentName, setStudentName] = useState('nome inicial')
+  const [students, setStudents] = useState([])
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString('pt-br', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    }
+
+    setStudents(prevState => [...prevState, newStudent])
+  }
+
 
   return (
     <div className="container">
       {/* uma variável não consegue refletir o estado na interface sozinha, por isso é preciso usar estado */}
       {/*aqui eu mostro o valor atualizado da variável que eu quero*/}
-      <h1>Nome: { studentName}</h1>
+      <header>
+        <h1>Nome: lista de presença</h1>
+        <div>
+          <strong>Charlie</strong>
+          <img src="https://github.com/charliebellow.png" alt="foto" />
+        </div>
+      </header>
       
       <input
         type="text" placeholder="Digite o nome"
@@ -21,11 +42,18 @@ export function Home() {
         onChange={e => setStudentName(e.target.value)}
       />
       
-      <button type="button">Adicionar</button>
+      <button type="button" onClick={handleAddStudent}>Adicionar</button>
+      {/* o map serve nesse caso para adicionar vários componentes */}
+      {
+        //para cada estudante renderiza um cartão 
+        students.map(student => (
+          <Card
+            key={student.time}
+            name={student.name}
+            time={student.time}
+          />))
       
-      <Card name="Rodrigo" time="10:55:24"/>
-      <Card name="João" time="11:23:45"/>
-      <Card />
+      }
     </div>
   )
 }
